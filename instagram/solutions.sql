eca7bc5b1de2319b929bea40e53b377eaf72c5b4
Sanaz <sanaz.azh@gmail.com>
2019-08-27 06:02:58 -0400

    add solutions

--- instagram/solutions.sql
+++ instagram/solutions.sql
@@ -15,4 +15,60 @@ LEFT JOIN photos
 ON photos.user_id = users.id
 WHERE photos.created_at is NULL;
 
+-- the most popular photo
+SELECT photos.id,
+       photos.image_url,
+       count(*) AS total,
+       users.username
+FROM photos
+INNER JOIN likes
+ON likes.photo_id = photos.id
+INNER JOIN users
+ON users.id = photos.user_id
+GROUP BY photos.id
+ORDER BY total desc
+LIMIT 10;
+
+-- how many times does an average user post?
+-- total photos/total users
+SELECT count(*) from photos;
+SELECT count(*) from users;
+
+SELECT count(*) / (SELECT count(*) from users) AS average from photos;
+SELECT 
+    (SELECT count(*) from photos) / (SELECT count(*) from users) AS average;
+
+-- find 5 most common hashtags
+SELECT 
+    tags.tag_name,
+    count(*) AS total
+FROM tags
+INNER JOIN photo_tags
+ON tags.id = photo_tags.tag_id
+GROUP BY tags.id
+ORDER BY total desc
+LIMIT 4;
+
+
+-- user with the highest posts
+SELECT  photos.user_id,
+        users.username,
+        count(*) AS total_posts
+FROM photos
+INNER JOIN users
+ON photos.user_id = users.id
+GROUP BY photos.user_id
+ORDER BY total_posts desc
+LIMIT 10;
+
+-- Find users who have liked every single photos on the site
+SELECT 
+    users.username,
+    users.id,
+    count(*) AS total_like
+from users 
+inner join likes on
+likes.user_id = users.id
+GROUP by users.id
+HAVING total_like = (SELECT count(*) from photos);
 
